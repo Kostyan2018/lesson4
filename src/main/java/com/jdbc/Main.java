50983ac893ed3bbb877787bd6963a3729a6d6240
Kostyan2018 <aub2011@i.ua>
2017-12-22 13:27:07 +0200

    Test

--- src/main/java/com/jdbc/Main.java
+++ src/main/java/com/jdbc/Main.java
@@ -1,40 +1,35 @@
 package com.jdbc;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class Main {
     private static String USERNAME = "root";
     private static String PASSWORD = "root";
     private static String URL = "jdbc:mysql://localhost:3306/mysql?useSSL=false";
-    //private static Connection connection;
 
     public static void main(String[] args) throws SQLException {
 
         DBProcessor db = new DBProcessor();
         Connection conn = db.getConnection(URL, USERNAME, PASSWORD);
-        String query = "select * from business.products where product_id = 13";
-        Statement statement = conn.createStatement();
-        ResultSet resSet = statement.executeQuery(query);
+        String query = "select * from business.products";
 
-        while (resSet.next()) {
-            int id;
-            String name;
-            double price;
-            int shopID;
+        PreparedStatement preStat = conn.prepareStatement(query);
+        ResultSet resSet = preStat.executeQuery();
 
-            id = resSet.getInt("product_Id");
-            name = resSet.getString("product_name");
-            price = resSet.getDouble("price");
-            shopID = resSet.getInt("shop_id");
-
-            Product product = new Product (id, name, price, shopID);
-            System.out.println(product);
+        String INSERT = "insert into business.products (product_name, price, shop_id) values(?, ?, ?)";
+        PreparedStatement prepInsert = conn.prepareStatement(INSERT);
+        prepInsert.setString(1 , "Ololo");
+        prepInsert.setDouble(2 , 98.0);
+        prepInsert.setInt(3,3);
+        prepInsert.execute();
+        
+        while (resSet.next()){
+            System.out.println(resSet.getInt("product_id") + " " + resSet.getString("product_name"));
         }
-
-        statement.close();
+        preStat.close();
         conn.close();
     }
 }
